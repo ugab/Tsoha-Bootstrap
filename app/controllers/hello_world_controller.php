06b69a6424eab2fe12c14b40e3d79d27fd395fac
ugab <ylinenjaakko@gmail.com>
2015-09-14 09:29:43 +0300

    mitään tehty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,32 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+       View::make('helloworld.html');
     }
+    
+    public static function lista(){
+      View::make('suunnitelmat/lista.html');
+    }
+    
+    public static function listaa_aanestykset(){
+        $aanestykset = aanestys::all();
+        
+    }
+
+    public static function muokkaa(){
+      View::make('suunnitelmat/muokkaa.html');
+    }
+
+    public static function uusi(){
+      View::make('suunnitelmat/uusi.html');
+    }    
+    
+    public static function aanestys(){
+      View::make('suunnitelmat/aanestys.html');
+    }        
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,24 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+    $routes->get('/lista', function() {
+    AanestysController::lista();
+    });
+    
+    $routes->get('/lista/:id', function($id){
+        AanestysController::nayta($id);
+    });
+    
+    $routes->get('/muokkaa', function() {
+      HelloWorldController::muokkaa();
+    });
+
+    
+    $routes->post('/aanestys', function(){
+      AanestysController::store();
+    });
+    
+    $routes->get('/uusi', function(){
+      AanestysController::uusi();
+    });  
